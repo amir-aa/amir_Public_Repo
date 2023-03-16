816d1788b7066db53636114cb6a639a7f59148bf
amir-aa <50378368+amir-aa@users.noreply.github.com>
2023-03-16 12:58:21 +0330

    Count and Say Problem

---
+++ count_say.py
@@ -0,0 +1,22 @@
+def countAndSay(n: int) -> str:
+    res = '1'
+    if n == 1:
+        return res
+        
+    for i in range(2,n+1):
+            ans = []
+            charPointer = res[0]
+            countPointer = 0
+            for char in res:
+                if char == charPointer:
+                    countPointer += 1
+                else:
+                    ans.append(str(countPointer))
+                    ans.append(charPointer)
+                    charPointer = char
+                    countPointer = 1
+            ans.append(str(countPointer))
+            ans.append(charPointer)
+            res = "".join(ans)
+    return res
+print(countAndSay(5))
