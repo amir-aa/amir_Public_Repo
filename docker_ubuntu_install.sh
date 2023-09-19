48fde21608e303d035d1fc5edafac879612cf265
AmirTheScientist <50378368+amir-aa@users.noreply.github.com>
2023-09-19 19:32:16 +0330

    Create docker_ubuntu_install.sh

---
+++ docker_ubuntu_install.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo systemctl status docker
