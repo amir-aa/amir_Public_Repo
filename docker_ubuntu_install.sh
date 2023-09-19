52b5650c3474cf7e448fcc31d765a642b8ca0f0d
AmirTheScientist <50378368+amir-aa@users.noreply.github.com>
2023-09-19 19:58:49 +0330

    Update docker_ubuntu_install.sh

--- docker_ubuntu_install.sh
+++ docker_ubuntu_install.sh
@@ -7,10 +7,9 @@ sudo apt install docker-ce
 sudo systemctl status docker
 #------------------------------18.04----------------------------------
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get install curl apt-transport-https ca-certificates software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
 apt-cache policy docker-ce
 apt install docker-ce
-sudo systemctl status docker
